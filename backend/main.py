e932c6da54e720a116cd31cb384403712389b133
BarsatKhadka <khadkabarsat598@gmail.com>
2025-07-12 07:21:18 -0500

    backend linux return active wm class

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from utils import get_os
 from monitor.monitorMain import monitor_main
-import shutil 
+import time
 
 app = FastAPI()
 os_name = get_os()
@@ -9,5 +9,6 @@ os_name = get_os()
 
 @app.get("/data")
 def read_data():
+    time.sleep(3)
     current_tab_info = monitor_main(os_name=os_name)
     return {"message": current_tab_info()} 
--- backend/monitor/monitorMain.py
+++ backend/monitor/monitorMain.py
@@ -1,4 +1,5 @@
 import shutil 
+import subprocess
 
 
 def monitor_main(os_name):
@@ -10,5 +11,12 @@ def monitor_main(os_name):
 def get_active_window_info_linux():
     if(shutil.which("xdotool") is None):
         return "No xdotool found"
-    else:
-        return "xodtool found"
+    if(shutil.which("xprop") is None):
+        return "No xprop found"
+    
+    try: 
+        active_window_id = subprocess.check_output(['xdotool', 'getactivewindow']).decode().strip()
+        active_wm_class = subprocess.check_output(['xprop','-id',active_window_id,'WM_CLASS']).decode()
+        return active_wm_class
+    except subprocess.CalledProcessError as e : 
+        return f"error {e}"
